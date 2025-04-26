50e8a1a7fba301d71369c740ecd0038491c01180
Mohammad Mowlavi <79265093+MohammadMow@users.noreply.github.com>
2025-04-27 02:53:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,3 @@ app = FastAPI()
 @app.get("/")
 async def redirect_to_secret():
     return RedirectResponse(url="http://127.0.0.1:5004/secret")
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
